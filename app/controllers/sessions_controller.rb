b50090b9be27ccbd4f5ffe7d4dca12a02da6c546
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-12 00:04:44 -0700

    #create will authenticate and assign session or redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,13 @@ class SessionsController < ApplicationController
   end
 
   def create #login
-
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy #logout
