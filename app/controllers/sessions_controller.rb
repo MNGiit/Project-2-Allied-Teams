30f55d45820a8ef0342e24d801ff3c199c25bd0c
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-12 00:01:22 -0700

    #new prepares new user or redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def new #signup
-
+    if !session[:user_id]
+      @user = User.new
+    else
+      redirect_to "/"
+    end
   end
 
   def create #login
