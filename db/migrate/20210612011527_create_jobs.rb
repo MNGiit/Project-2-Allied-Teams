35a489d176b73fd4fb60db246802eae5282cabc8
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-11 18:34:32 -0700

    job has these attributes: location, department, function, description, visible

--- db/migrate/20210612011527_create_jobs.rb
+++ db/migrate/20210612011527_create_jobs.rb
@@ -1,6 +1,11 @@
 class CreateJobs < ActiveRecord::Migration[6.1]
   def change
     create_table :jobs do |t|
+      t.string :location
+      t.string :department
+      t.string :function
+      t.string :description
+      t.boolean :visible, :default => true
 
       t.timestamps
     end
