84b55978ebe5f8c1345bd05b80c164849fc014eb
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-13 22:45:54 -0700

    jobs will have #index, #show, #new, #create, #jobs_params

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,23 @@
 class JobsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def job_params
+
+  end
 end
