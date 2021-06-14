d00f91294cbc0afa7750df2bad449edb399e055c
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-13 20:09:13 -0700

    #new will prepare new user or redirect if there is a session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+  def new # signup
+    if !session[:user_id]
+      @user = User.new
+    else
+      redirect_to "/" # root_path
+    end
+  end
+
 end
