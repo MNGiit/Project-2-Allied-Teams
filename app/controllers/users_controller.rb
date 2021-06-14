a050bd4b13b0f3a58a209400ca8f2a8d0c6c0828
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-13 22:30:13 -0700

    user params will permit email, first and last name, filling position, and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-
+    params.require(:user).permit(:email, :first_name, :last_name, :filling_position, :password)
   end
-
 end
