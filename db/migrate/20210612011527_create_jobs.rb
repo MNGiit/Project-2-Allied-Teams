ac547793662eb8d03d468c6cb711a41a3ed7e3fb
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-17 06:20:32 -0700

    add more attributes

--- db/migrate/20210612011527_create_jobs.rb
+++ db/migrate/20210612011527_create_jobs.rb
@@ -1,10 +1,21 @@
 class CreateJobs < ActiveRecord::Migration[6.1]
   def change
     create_table :jobs do |t|
+      t.string :title
+      t.integer :code
+      t.string :description
+      t.string :industry
+      t.string :function
       t.string :location
       t.string :department
-      t.string :function
-      t.string :description
+      t.integer :minimum_experience_level
+      t.integer :experience_level
+      t.integer :positions
+      t.integer :annual_salary_from
+      t.integer :annual_salary_upto
+      t.string :currency
+      t.boolean :show_salary_details, :default => true
+
       t.boolean :visible, :default => true
 
       # could make another model called applicants and join the two
