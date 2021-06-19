5eabe94a1f3e1fa62dd27f1a95b4928c4796e660
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-18 22:53:31 -0700

    array for autocomplete

---
+++ app/javascript/jobsnew.js
@@ -0,0 +1,20 @@
+let titles = [
+  "Web Developer",
+  "Web Designer",
+  "Graphics Designer",
+  "Graphics Artist",
+  "3D Graphics Artist",
+  "Shaders Developer",
+  "JavaScript Developer",
+  "Ruby Developer",
+  "Python Developer",
+  "C# Developer",
+  "C++ Developer",
+  "Java Developer",
+  "Game Developer",
+  "Software Developer",
+  "Software Engineer",
+  "Data Scientist",
+  "IT Professional",
+  "Computer Engineer"
+]
