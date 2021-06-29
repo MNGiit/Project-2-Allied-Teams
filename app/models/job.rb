2442cb2cddba87c9da57f6269d7f83f3c8428db1
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-29 12:13:44 -0700

    add scope to filter by function

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ApplicationRecord
+  # this works, try with :scope
+  # Job.where(["location = ?", "Washington"])
+  # Job.where(["department = ?", "IT"])
+  # scope :costs_more_than, ->(amount) { where("price > ?", amount) }
+  scope :filter_function, ->(search) { where("function = ?", search)} # Job.where(["function = ?", "Web Developer"])
 end
