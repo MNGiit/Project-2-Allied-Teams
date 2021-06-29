5e7647c884e383b02c8811cabe9d61144625e295
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-29 12:15:42 -0700

    add scope to filter by department

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,6 @@
 class Job < ApplicationRecord
-  # this works, try with :scope
-  # Job.where(["department = ?", "IT"])
-  # scope :costs_more_than, ->(amount) { where("price > ?", amount) }
+  # scope :costs_more_than, ->(amount) { where("price > ?", amount) } # https://guides.rubyonrails.org/active_record_querying.html#scopes
   scope :filter_function, ->(search) { where("function = ?", search)} # Job.where(["function = ?", "Web Developer"])
   scope :filter_location, ->(search) { where("location = ?", search)} # Job.where(["location = ?", "Washington"])
+  scope :filter_department, ->(search) { where("department = ?", search)} # Job.where(["department = ?", "IT"])
 end
