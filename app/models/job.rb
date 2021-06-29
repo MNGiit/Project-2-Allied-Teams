29d97307d38f49ec06c463df8368ea653d24b18b
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-29 12:14:24 -0700

    add scope to filter by location

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ApplicationRecord
   # this works, try with :scope
-  # Job.where(["location = ?", "Washington"])
   # Job.where(["department = ?", "IT"])
   # scope :costs_more_than, ->(amount) { where("price > ?", amount) }
   scope :filter_function, ->(search) { where("function = ?", search)} # Job.where(["function = ?", "Web Developer"])
+  scope :filter_location, ->(search) { where("location = ?", search)} # Job.where(["location = ?", "Washington"])
 end
