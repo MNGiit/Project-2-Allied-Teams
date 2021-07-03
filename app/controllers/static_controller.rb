f14abb69b2c6ec5ed35177a80b803b9bad6642b8
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-07-02 21:45:31 -0700

    check if user session is filling a position

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,5 @@
 class StaticController < ApplicationController
   def index
-
+    @filling_position = User.find_by(id: session[:user_id]).filling_position if User.find_by(id: session[:user_id])
   end
 end
