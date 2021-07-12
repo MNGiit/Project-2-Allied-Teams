b7314a131b2c41d44a43006c8d0e155688a22e89
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-07-11 20:34:20 -0700

    job has many applicants

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
+  has_many :applicants
   # scope :costs_more_than, ->(amount) { where("price > ?", amount) } # https://guides.rubyonrails.org/active_record_querying.html#scopes
   scope :filter_function, ->(search) { where("function = ?", search)} # Job.where(["function = ?", "Web Developer"])
   scope :filter_location, ->(search) { where("location = ?", search)} # Job.where(["location = ?", "Washington"])
