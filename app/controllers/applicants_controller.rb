a2b9e99fc0fc2242fdc7d6e2db1961372b33c09f
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-07-12 03:05:23 -0700

    permit user and job id

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -3,4 +3,18 @@ class ApplicantsController < ApplicationController
     @user = User.find_by(id: session[:user_id]) if session[:user_id]
     @jobs = Job.all
   end
+
+  def create
+    #
+    @applicant = Applicant.create(applicant_params)
+    binding.pry
+    # redirect_to user_jobs_path(session[:user_id])
+    redirect_to applicants_path
+  end
+
+  private
+
+  def applicant_params
+    params.require(:applicant).permit(:job_id, :user_id)
+end
 end
